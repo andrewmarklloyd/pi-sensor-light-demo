84b450a2cbde903c191bc68ab6f7f4eb01e1eb20
Andrew Lloyd <andrew85.lloyd@gmail.com>
2021-03-08 21:24:04 -0800

    Update main.py

--- main.py
+++ main.py
@@ -18,3 +18,18 @@ if device:
   device.toggle()
 else:  
   print(f'Could not find {device_name}')
+
+import RPi.GPIO as GPIO #import the GPIO library
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+while True:
+    if GPIO.input(8):
+       print("Door is open")
+       time.sleep(2)
+    if GPIO.input(8) == False:
+       print("Door is closed")
+       time.sleep(2)
